b69af665d3b8616fb6dc419659f7585d016231c6
tgd <zjrzpp0414@163.com>
2019-07-05 20:31:11 +0800

    aa

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ npm install
 npm start
 
 ```
+
+## 问题
+### HTTP options方法的作用
--- index.js
+++ index.js
@@ -5,10 +5,19 @@ var app = new Koa()
 // 创建一个路由实例
 var router = new Router()
 
+// 创建一个以/users前缀的路由实例
+const usersRouter = new Router({prefix: '/users'})
+
 router.get('/', (ctx, next) => {
   ctx.body = '哈哈'
 })
-
+usersRouter.get('/', (ctx)=> {
+  ctx.body = '这是用户页面'
+})
+usersRouter.get('/:id', (ctx)=> {
+  ctx.body = `这是用户${ctx.params.id}的页面`
+})
+app.use(usersRouter.routes())
 app.use(router.routes())
 
 app.listen(3000)
