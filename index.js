082ee61996ea51751f37b335c97b58fa7c6057c9
zhangpanpan <zjrzpp0414@163.com>
2019-07-06 08:35:06 +0800

    xx

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ var Koa = require('koa')
 var app = new Koa()
 
 app.use(async (ctx, next) => {
-  ctx.body = 'hello zhihu'
+  console.log(ctx)
   await next()
 })
 
+// 路由中间件的作用：
+// 1. 处理不同的URL
+// 2. 处理不同的HTTP方法
+// 3. 解析URL上的参数
 
 
 app.listen(3000)
